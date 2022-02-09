c322cf8a650664aa1f80a3777adc43d26778df81
Daniel He <dhe30@stuy.edu>
2022-02-09 10:35:30 -0500

    RecursionClasswork partialSum tested and it is working

--- classwork/partial.java
+++ classwork/partial.java
@@ -8,7 +8,7 @@ public class partial{
     return partialSum(start + 1, arr, targetValue - arr[start]) || partialSum(start + 1, arr, targetValue);
   }
   public static void main(String[] args){
-    int[] arr = {2,3,4,5};
-    System.out.println(partialSum(0,arr,7));
+    int[] arr = {2,3,4};
+    System.out.println(partialSum(0,arr,10));
   }
 }
