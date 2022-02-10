f1873b3662cf52328aaaed6c6d213d8d57d24388
Daniel He <dhe30@stuy.edu>
2022-02-10 10:58:00 -0500

    RecursionClasswork created inital sum6

--- classwork/partial.java
+++ classwork/partial.java
@@ -37,6 +37,17 @@ public class partial{
 
   }
 
+  public boolean groupSum6(int start, int[] nums, int target) {
+    if (target == 0 && start == nums.length){
+      return true;
+    } else if (start == nums.length){
+      return false;
+    } else if(nums[start]%6 == 0){
+      return (groupSum6(start + 1, nums, target - nums[start]));
+    }
+    return (groupSum6(start + 1, nums, target - nums[start])) || (groupSum6(start + 1, nums, target));
+  }
+
   public static void main(String[] args){
     int[] arr = {1,2,1,3,7};
     System.out.println(partialSum(0,arr,9));
