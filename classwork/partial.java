fb763f06f99bae045ea936f448dfa838be4dda42
Daniel He <dhe30@stuy.edu>
2022-02-09 20:48:08 -0500

    RecursionClasswork created initial splitArray

--- classwork/partial.java
+++ classwork/partial.java
@@ -16,12 +16,19 @@ public class partial{
     }
     return sumArray(nums, start + 1, sum + nums[start]);
   }
-  // public static boolean splitArray(int[] nums) {
-  //   splitArray(nums, 0, 0);
-  // }
-  // public static boolean splitArray(int[] nums, int sum, int start, int[] nums1, int[]nums2) {
-  //   return (nums,sum)
-  // }
+  public static boolean splitArray(int[] nums) {
+    splitArray(nums, 0, 0, 0);
+  }
+  public static boolean splitArray(int[] nums, int start, int nums1, int nums2) {
+    if (start != nums.length){
+      return splitArray(nums,start + 1,nums1+nums[start],nums2)||splitArray(nums,start + 1,nums1,nums2+nums[start]);
+    } else {
+      if (nums1 == nums2){
+        return true;
+      }
+      return false;
+    }
+  }
   public static void main(String[] args){
     int[] arr = {1,1,1,0};
     System.out.println(sumArray(arr,0,0));
