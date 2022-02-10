580f9c1f839e737e46d29549d13bc7f86e3c4a7e
Daniel He <dhe30@stuy.edu>
2022-02-09 20:51:16 -0500

    RecursionClasswork wow inital tests somehow work, will now test splitArray on codingBat

--- classwork/partial.java
+++ classwork/partial.java
@@ -17,7 +17,7 @@ public class partial{
     return sumArray(nums, start + 1, sum + nums[start]);
   }
   public static boolean splitArray(int[] nums) {
-    splitArray(nums, 0, 0, 0);
+    return splitArray(nums, 0, 0, 0);
   }
   public static boolean splitArray(int[] nums, int start, int nums1, int nums2) {
     if (start != nums.length){
@@ -26,11 +26,11 @@ public class partial{
       if (nums1 == nums2){
         return true;
       }
-      return false;
+        return false;
     }
   }
   public static void main(String[] args){
-    int[] arr = {1,1,1,0};
-    System.out.println(sumArray(arr,0,0));
+    int[] arr = {1,2,1,3,7};
+    System.out.println(splitArray(arr));
   }
 }
