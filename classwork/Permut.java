58db191934c591896c1e195f5ee9497d0014abac
Daniel He <dhe30@stuy.edu>
2022-02-13 19:55:10 -0500

    i

---
+++ classwork/Permut.java
@@ -0,0 +1,23 @@
+public class Permut{
+  //current is the current index, which starts at 0 when you invoke the method in your main
+            //the sentence starts at "", the recursion concatenates the words
+            //base case should print the sentence
+  public static void permute(String[][]lists, String sentence, int current){
+    if (current >= lists.length){
+      System.out.println(sentence);
+    } else {
+      for (int i = 0; i < lists[current].length; i++){
+        permute(lists, sentence + " " +lists[current][i], current + 1);
+      }
+    }
+  }
+  public static void main(String[] args){
+    String[][]wordlists = {
+             {"The dog", "The pro skater"},
+             {"plays" , "empathises with" },
+             {"chess", "ball", "the voiceless masses"}
+           };
+           permute(wordlists,"",0);
+
+  }
+}
