2708e8ba96124ffe0fc353b082bab624e8ed1f63
Daniel He <dhe30@stuy.edu>
2022-02-18 11:00:05 -0500

    classwork.java

---
+++ classwork/maize.java
@@ -0,0 +1,30 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+public class maize{
+  public static char[][] masePutterInner(String filee) throws FileNotFoundException{
+    ArrayList<String> store = new 
+    File file = new File(filee);
+    Scanner input = new Scanner(file);
+    while (input.hasNextLine()){
+      store += input.nextLine() + "/n";
+      length += 1;
+    }
+    input.close();
+  }
+  public static void masePrinter(String filee) throws FileNotFoundException{
+    File file = new File(filee);
+    Scanner input = new Scanner(file);
+    while (input.hasNextLine()){
+      System.out.println(input.nextLine());
+    }
+    input.close();
+  }
+  public static void main(String[] args){
+    try{
+      mase("maze.txt");
+    } catch(FileNotFoundException e){
+
+    }
+  }
+}
