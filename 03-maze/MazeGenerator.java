2d6a6753acd2057e743a45c829c68630886a9242
Daniel He <dhe30@stuy.edu>
2022-03-02 10:41:31 -0500

    made mase generator not working tho

--- 03-maze/MazeGenerator.java
+++ 03-maze/MazeGenerator.java
@@ -1,8 +1,11 @@
 public class MazeGenerator{
   int[][] coor = {{0,1},{0,-1},{1,0},{-1,0}};
   public static void generate(char[][]maze,startrow,startcol){
+    int openSpaces = 0;
     for (int i = 0; i < coor.length; i++){
-      for (int a)
+      for (int a = 0; a < 2; a++){
+        if(maze[startrow + coor[i][a]][startcol])
+      }
     }
   }
 }
