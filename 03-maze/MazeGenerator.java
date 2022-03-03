fcc1f8391641584049e3bd1d65588581a8d39765
Daniel He <dhe30@stuy.edu>
2022-03-03 11:00:56 -0500

    not randomized yet

--- 03-maze/MazeGenerator.java
+++ 03-maze/MazeGenerator.java
@@ -1,11 +1,36 @@
 public class MazeGenerator{
-  int[][] coor = {{0,1},{0,-1},{1,0},{-1,0}};
-  public static void generate(char[][]maze,startrow,startcol){
-    int openSpaces = 0;
-    for (int i = 0; i < coor.length; i++){
-      for (int a = 0; a < 2; a++){
-        if(maze[startrow + coor[i][a]][startcol])
+  public static final int[][] coor = {{0,1},{0,-1},{1,0},{-1,0}};
+  public static void generate(char[][]maze,int startrow,int startcol){
+    if(startrow > 0 && startrow < maze.length - 1 && startcol > 0 && startcol < maze[0].length - 1){
+      int openSpaces = 0;
+      for (int i = 0; i < coor.length; i++){
+        if(maze[startrow + coor[i][0]][startcol + coor[i][1]] == ' '){
+          openSpaces += 1;
+        }
+      }
+      if (openSpaces <= 2){
+        maze[startrow][startcol] = ' ';
+        int row = (int)(Math.random()*4);
+        generate(maze, startrow + coor[row][0], startcol + coor[row][1]);
       }
     }
+  }
+  public static void main (String[] args){
+    char[][] c = new char[8][8];
+    for (int i = 0; i < c.length; i++){
+      System.out.println((int)(Math.random()*4));
+      for (int a = 0; a < c[i].length; a++){
+        c[i][a] = '#';
+      }
+    }
+    generate(c, 6, 6);
+    String ans = "";
+    for (int i = 0; i < c.length; i++){
+      for (int a = 0; a < c[i].length; a++){
+        ans += c[i][a];
+        }
+        ans += "\n";
+      }
+      System.out.println(ans);
   }
 }
