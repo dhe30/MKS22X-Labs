5a5be8226b492c2c0154dec0e6dbd152dbfb55f1
Daniel He <dhe30@stuy.edu>
2022-03-04 11:01:57 -0500

    nowhere

--- 03-maze/MazeGenerator.java
+++ 03-maze/MazeGenerator.java
@@ -1,3 +1,6 @@
+import java.io.*;
+import java.util.*;
+
 public class MazeGenerator{
   public static final int[][] coor = {{0,1},{0,-1},{1,0},{-1,0}};
   public static void generate(char[][]maze,int startrow,int startcol){
@@ -11,8 +14,23 @@ public class MazeGenerator{
       if (openSpaces <= 2){
         maze[startrow][startcol] = ' ';
         int row = (int)(Math.random()*4);
+        //generate(maze, startrow + coor[row][0], startcol + coor[row][1]);
+        //if(maze[startrow + coor[row][0]][startcol + coor[row][1]])
         generate(maze, startrow + coor[row][0], startcol + coor[row][1]);
-        if()
+        if(maze[startrow + coor[row][0]][startcol + coor[row][1]] != ' '){
+            maze[startrow][startcol] = '.';
+        }
+        // int[] maths = [3];
+        // for (int i = 0; i < 4; i++){
+        //   if(i == row){
+        //     i--
+        //   }
+        // }
+        // row = (int)(Math.random()*3);
+        // generate(maze, startrow + coor[maths.get(row)][0], startcol + coor[maths.get(row)][1]);
+        // if(maze[startrow + coor[maths.get(row)][0]][startcol + coor[maths.get(row)][1]] != ' '){
+        //     maze[startrow][startcol] = '.';
+        // }
       }
     }
   }
