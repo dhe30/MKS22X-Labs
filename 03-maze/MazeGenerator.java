e2cdce48c0dc0b32c343943efe9bd2de26233deb
Daniel He <dhe30@stuy.edu>
2022-03-05 14:55:28 -0500

    made function that returns random directions

--- 03-maze/MazeGenerator.java
+++ 03-maze/MazeGenerator.java
@@ -3,6 +3,25 @@ import java.util.*;
 
 public class MazeGenerator{
   public static final int[][] coor = {{0,1},{0,-1},{1,0},{-1,0}};
+  public static int[] fromFour(){
+    // returns an int array with 0-3 in a random order
+    ArrayList<Integer> a = new ArrayList<Integer>();
+    for (int i = 0; i < 4; i++){
+      a.add(i);
+    }
+    int[] b = new int[4];
+    return fromFour(a,b,0);
+  }
+  public static int[] fromFour(ArrayList<Integer> a, int[] b, int c){
+    if(a.size() == 0){
+      return b;
+    } else {
+      int r = (int)(Math.random()*a.size());
+      b[c] = a.get(r);
+      a.remove(r);
+      return fromFour(a,b,c+1);
+    }
+  }
   public static void generate(char[][]maze,int startrow,int startcol){
     if(startrow > 0 && startrow < maze.length - 1 && startcol > 0 && startcol < maze[0].length - 1){
       int openSpaces = 0;
@@ -11,33 +30,32 @@ public class MazeGenerator{
           openSpaces += 1;
         }
       }
-      if (openSpaces <= 2){
+      if (openSpaces < 2){
         maze[startrow][startcol] = ' ';
-        int row = (int)(Math.random()*4);
-        //generate(maze, startrow + coor[row][0], startcol + coor[row][1]);
-        //if(maze[startrow + coor[row][0]][startcol + coor[row][1]])
-        generate(maze, startrow + coor[row][0], startcol + coor[row][1]);
-        if(maze[startrow + coor[row][0]][startcol + coor[row][1]] != ' '){
-            maze[startrow][startcol] = '.';
+        int[] row = fromFour();
+        generate(maze, startrow - 1, startcol);
+        if(maze[startrow - 1][startcol] != ' '){
+          generate(maze, startrow, startcol - 1);
         }
-        // int[] maths = [3];
-        // for (int i = 0; i < 4; i++){
-        //   if(i == row){
-        //     i--
+        // generate(maze, startrow + coor[row[0]][0], startcol + coor[row[0]][1]);
+        // if(maze[startrow + coor[row[0]][0]][startcol + coor[row[0]][1]] != ' '){
+        //   System.out.println("row: " + startrow +  " col: " + startcol);
+        //   generate(maze, startrow + coor[row[1]][0], startcol + coor[row[1]][1]);
+        //   if(maze[startrow + coor[row[1]][0]][startcol + coor[row[1]][1]] != ' '){
+        //     System.out.println("2row: " + startrow +  " col: " + startcol);
+        //     generate(maze, startrow + coor[row[2]][0], startcol + coor[row[2]][1]);
+        //     if(maze[startrow + coor[row[2]][0]][startcol + coor[row[2]][1]] != ' '){
+        //       generate(maze, startrow + coor[row[3]][0], startcol + coor[row[3]][1]);
+        //     }
         //   }
         // }
-        // row = (int)(Math.random()*3);
-        // generate(maze, startrow + coor[maths.get(row)][0], startcol + coor[maths.get(row)][1]);
-        // if(maze[startrow + coor[maths.get(row)][0]][startcol + coor[maths.get(row)][1]] != ' '){
-        //     maze[startrow][startcol] = '.';
-        // }
+
       }
     }
   }
   public static void main (String[] args){
     char[][] c = new char[8][8];
     for (int i = 0; i < c.length; i++){
-      System.out.println((int)(Math.random()*4));
       for (int a = 0; a < c[i].length; a++){
         c[i][a] = '#';
       }
