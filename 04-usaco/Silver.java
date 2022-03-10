6163bd8064b8ecc31164dfd6a3da482b39ba4eb5
Daniel He <dhe30@stuy.edu>
2022-03-09 22:11:28 -0500

    made test grid for silver but i dont see the pattern

---
+++ 04-usaco/Silver.java
@@ -0,0 +1,32 @@
+public class Silver{
+  public static void dug(int[][] a, int startRow, int startCol, int max){
+    if (startRow >= 0 && startRow < a.length && startCol >= 0 && startCol < a[0].length){
+      if(max == 0){
+        a[startRow][startCol] += 1;
+      } else {
+
+        dug(a, startRow - 1, startCol, max - 1);
+        dug(a, startRow, startCol - 1, max - 1);
+        dug(a, startRow + 1, startCol, max - 1);
+        dug(a, startRow, startCol + 1, max - 1);
+      }
+    }
+  }
+  public static void main(String[] args){
+    int[][] a = new int[11][11];
+    for (int[] b : a){
+      for (int i = 0; i < b.length; i++){
+        b[i] = 0;
+      }
+    }
+    dug(a,5,5,4);
+    String ans = "";
+    for (int[] b : a){
+      for (int c : b){
+        ans += ("   " + c);
+      }
+      ans += "\n";
+    }
+    System.out.println(ans);
+  }
+}
