743cf2061b56df8711d6283a5971ee1f909670cc
Daniel He <dhe30@stuy.edu>
2022-03-15 11:01:41 -0400

    yes

--- 05-quick/Preliminary.java
+++ 05-quick/Preliminary.java
@@ -18,21 +18,46 @@ public class Preliminary{
 */
   public static int partition ( int [] data, int start, int end){
     int picker = (int)(Math.random() * end - start + 1) + start;
+    System.out.println(data[picker] + "end: " + end);
     swarp(data, start, picker);
+    int i = 0;
+    boolean red = false;
     while(start != end){
-      if (data[start + 1] > data[start]){
+      if (data[start + 1] > data[start - i]){
         swarp(data, start + 1, end);
         end -= 1;
-      } else {
-        swarp(data, start + 1, start);
+        System.out.println(Arrays.toString(data) + "start: " + start + " end: " + end);
+      } else if (data[start + 1] < data[start - i]){
         start += 1;
+        i++;
+        System.out.println(Arrays.toString(data)  + "start: " + start + " end: " + end);
+      } else {
+        if (red){
+          start += 1;
+          red = false;
+          i++;
+          System.out.println(Arrays.toString(data)  + "start: " + start + " end: " + end);
+        } else {
+          swarp(data, start + 1, end);
+          end -= 1;
+          red = true;
+          System.out.println(Arrays.toString(data)  + "start: " + start + " end: " + end);
+        }
       }
     }
+
+    //swarp(data, start + 1, start - i);
     return start;
   }
   public static void main(String[] args){
-    int[] a = {1,2,3,4,5,6,7,8,99};
-    System.out.println(partition(a,0,a.length - 1));
-    System.out.println(Arrays.toString(a));
+    // int[] a = {1,2,3,4,5,99,99,99,99};
+    // System.out.println(partition(a,0,a.length - 1));
+    // System.out.println(Arrays.toString(a));
+    int [] data = new int[] {993,994,995,4,3,2,1,0,997,998,999};
+    System.out.println("Original: "+Arrays.toString(data));
+    int pivot = partition( data , 3, 7);
+    System.out.println("Pivot value: "+data[pivot]+ ", Pivot index: "+pivot);
+    System.out.println("Modified: "+Arrays.toString(data));
+    System.out.println();
   }
 }
