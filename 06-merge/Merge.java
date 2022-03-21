9b3521edef685de4cda597b109f72cc8d705d994
Daniel He <dhe30@stuy.edu>
2022-03-21 11:02:22 -0400

    formatted

---
+++ 06-merge/Merge.java
@@ -0,0 +1,18 @@
+public class Merge{
+  public static int [] merge(int [] left, int[] right){
+          //return a new array that is the merged version of left and right
+        }
+
+  public static void mergesort(data){
+    int [] temp  =  mergeSortH(data);
+      copy temp into data
+  }
+
+ public static int[] mergesortH(data){
+          if more than 1 element{
+            left = copy half of data
+            right = copy other half of data
+            return merge(mergesort(left),mergesort(right))
+          }
+        }
+}
