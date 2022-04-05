05d7c59b4dae1e815c4273050fce71a224085459
Daniel He <dhe30@stuy.edu>
2022-04-05 10:57:04 -0400

    .

---
+++ classwork/taat.java
@@ -0,0 +1,8 @@
+import java.util.*;
+import java.io.*;
+public class taat{
+  public static void main(String[] args){
+    Queue<int[]> a = new ArrayDeque<int[]>();
+    int[] b = new int[]{1,2};
+  }
+}
